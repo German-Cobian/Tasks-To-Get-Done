b881fc6e2d25ed6e31f1aeb5f24ef63207d1f121
German Renato Cobian <germancobian@hotmail.com>
2023-05-30 00:28:37 -0700

    Create loadActivitiesList() and set to operate upon DOM content loaded + move display logic from index file to newly created app-display

--- src/app-functionality.js
+++ src/app-functionality.js
@@ -1,19 +1,31 @@
+import renderList from './app-display';
+
 // #1 Check display of dummy variables
 
 //const activities = [
 //  { description: 'Un-clog the toilet', completed: false, index: 2 },
 //  { description: 'Complain to the neighbor about his brats', completed: false, index: 4 },
-//  { description: 'De-flea the dog', completed: true, index: 1 },
+////  { description: 'De-flea the dog', completed: true, index: 1 },
 //  { description: 'Dish-out ethique lessons to porcupine', completed: true, index: 3 },
 //];
 
 // #4 Create the activities array where values will truly be stored
+// eslint-disable-next-line import/no-mutable-exports
 let activities = [];
+
+// #6 load activities inmediately upon page load
+const loadActivitiesList = () => {
+  let loadActivities = JSON.parse(localStorage.getItem('activities'));
+  if (loadActivities == null) {
+    loadActivities = [];
+  }
+  activities = loadActivities;
+  renderList(activities);
+};
 
 // #5 Add new activity into localStorage (set the updated activities array)
 const archiveActivities = () => {
   localStorage.setItem('activities', JSON.stringify(activities));
-  console.log(activities[0].description, activities[0].completed, activities[0].index);
 };
 
 // #3 Add inputted activity to the activities array
@@ -34,4 +46,5 @@ const assignIndexToActivity = (description) => {
 export {
   activities,
   assignIndexToActivity,
+  loadActivitiesList,
 };
